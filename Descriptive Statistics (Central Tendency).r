b11e3c18660e153c4a5691c8215ccf07e94bd429
Ta'fif Lukman Afandi <82430379+Tafif04@users.noreply.github.com>
2022-11-27 20:57:09 +0700

    Update Descriptive Statistics (Central Tendency).r

--- Descriptive Statistics (Central Tendency).r
+++ Descriptive Statistics (Central Tendency).r
@@ -1,18 +1,19 @@
-IMPORT DATASET
+# IMPORT DATASET
 dt = read.csv("./dataset/iris_csv.csv")
 print(head(dt))
-MEAN: SEPAL LENGTH (with NA)
+# MEAN: SEPAL LENGTH (with NA)
 mean(dt$sepallength, na.rm=TRUE)
-MEAN: SEPAL LENGTH (without NA)
+# MEAN: SEPAL LENGTH (without NA)
 mean(dt$sepallength)
-MEDIAN: SEPAL LENGTH (with NA)
+
+# MEDIAN: SEPAL LENGTH (with NA)
 median(dt$sepallength, na.rm = TRUE)
-MEDIAN: SEPAL LENGTH (without NA)
+# MEDIAN: SEPAL LENGTH (without NA)
 median(dt$sepallength)
 
 mode = function(x){
     uniqx = unique(x)
     uniqx[which.max(tabulate(match(x, uniqx)))]
 }
-MODE: SEPAL LENGTH
+#MODE: SEPAL LENGTH
 mode(dt$sepallength)
